bd9993043e22a9a2edfed7d14537e482f2885999
ricardorompar <ricardo.romero@hashicorp.com>
2025-03-06 00:58:13 +0100

    new outputs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "ip" {
   value = aws_instance.web.public_ip
 }
 
-output "boundary_connect" {
-  value = "boundary connect evolutio.ssh.terramino"
+output "boundary_connect_alias" {
+  value = "boundary connect ssh evolutio.ssh.terramino"
+}
+
+output "boundary_connect_target_id" {
+  value = "boundary connect ssh -target-id ${boundary_target.ssh.id}"
 }
