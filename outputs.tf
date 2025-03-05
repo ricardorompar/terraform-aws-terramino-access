3f0a12754b7552f379dcadd75d82f214671e2887
ricardorompar <ricardo.romero@hashicorp.com>
2025-03-05 22:36:32 +0100

    added boundary connect output

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,6 @@ output "ip" {
   value = aws_instance.web.public_ip
 }
 
-output "key_test" {
-  value     = data.vault_kv_secret_v2.vault_ca.data["ca_public_key"]
-  sensitive = true
+output "boundary_connect" {
+  value = "boundary connect evolutio.ssh.terramino"
 }
