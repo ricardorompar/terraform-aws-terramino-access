5648210a2cd1fc2bba3a7b7d50c7adcb95a78e36
ricardorompar <ricardo.romero@hashicorp.com>
2025-03-05 22:16:22 +0100

    removed aws key pair and changed main accordingly

--- main.tf
+++ main.tf
@@ -52,7 +52,6 @@ resource "aws_instance" "web" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t3.small"
   security_groups = [aws_security_group.allow_web_traffic.name]
-  key_name        = aws_key_pair.ssh_key_pair.key_name
 
   user_data                   = data.cloudinit_config.content.rendered
   user_data_replace_on_change = true
