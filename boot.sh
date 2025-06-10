3acea1db1d8ff7941afaf447099338498358ed4f
ricardorompar <ricardo.romero@hashicorp.com>
2025-06-10 23:03:43 +0200

    updated boot.sh script

--- boot.sh
+++ boot.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+set -euo pipefail
 
-echo "Running boot.sh" >> /home/ubuntu/log.txt
-echo "This is the project ID: ${project_id}" >> /home/ubuntu/log.txt
-echo "==============================" >> /home/ubuntu/log.txt
+LOGFILE="/var/log/terramino-cloud-init.log"
 
+function log {
+  local level="$1"
+  local message="$2"
+  local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  local log_entry="$timestamp [$level] - $message"
+
+  echo "$log_entry" | tee -a "$LOGFILE"
+}
+
+log "INFO" "Beginning custom_data script. This is the project ID: ${project_id}"
+
+log "INFO" "Installing required packages"
 curl -OL https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.23.1.linux-amd64.tar.gz
 
+log "INFO" "Setting up environment variables for go application"
 export PATH=$PATH:/usr/local/go/bin
 export HOME="/tmp/home"
 export GOPATH="/tmp/go"
@@ -17,6 +29,7 @@ export HCP_CLIENT_SECRET=${client_secret}
 export HCP_ORGANIZATION_ID=${org_id}
 export HCP_PROJECT_ID=${project_id}
 
+log "INFO" "Cloning the terramino-go repository"
 cd /tmp
 git clone https://github.com/hashicorp-education/terramino-go.git
 cd terramino-go
@@ -24,4 +37,5 @@ cd terramino-go
 # Temporarily checkout a previous version of terramino-go
 git checkout 58d39908e07424cba3b0c2f2d9588d9cebfa476b
 
+log "INFO" "Building the terramino-go application"
 APP_NAME=${app_name} TERRAMINO_PORT=${port} nohup go run main.go &
